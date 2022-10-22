cfc4c5c854da5e20f3ef52ba05aad8a0dd032254
Eder P Ferreira <eder.ferreira.ads@gmail.com>
2022-10-22 11:07:39 -0400

    correção

--- banco/Untitled.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO tbUsuario(nome, email, telefone, senha) values 
-('MARIA PIRES','mariap@hotmail.com','6599999 7743','mariap'),
-('JOSE DA SILVA SAURO','jose@hotmail.com','6599999 222','admin'),
-('KARLA FILISBINA','filisbina@gmail.com','6599999 3333','admin'),
-('PAULA CATAPREGO','catapaula@gmail.com','6599999 4444','admin'),
-('EVANILDO SANTIAGO','evasantiago@hotmail.com','6599999 5555','admin'),
-('FRANCISCO DE SOUSA','sousa@hotmail.com','6599999 7666','admin'),
-('FRANCIS OLEGARIO','olegario@hotmail.com','6599999 7890','admin'),
-('ADRIANA FERREIRA SILVA','adriana@hotmail.com','6599999 1234','admin');
